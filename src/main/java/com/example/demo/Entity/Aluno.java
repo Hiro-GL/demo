4ee66aece75ac654328b4d4ee2189b9d7bbfbd6d
Hiro-GL <gabriellimaa.900@gmail.com>
2025-08-21 22:01:12 -0300

    commit

---
+++ src/main/java/com/example/demo/Entity/Aluno.java
@@ -0,0 +1,73 @@
+package com.example.demo.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+    private Date dataNasc;
+
+    public Aluno(String nome, String email, String senha, Date dataNasc) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.dataNasc = dataNasc;
+    }
+
+    public Aluno(){
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+}
